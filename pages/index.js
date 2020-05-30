7eba1c75cc6d704ca3d7a6082066c7c5f7df1442
Andrew Jones <aej11@hotmail.com>
2020-05-30 01:02:15 -0400

    Added profile picture and some base styles

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,19 @@ export default function Home() {
       </Head>
 
       <main>
-
+        <div className="bg-gray-800 w-screen h-screen flex items-center justify-center">
+            <div className="intro bg-gray-200 w-4/5 rounded-t-lg md:rounded-l-md md:rounded-t-none">
+                <h1 className="m-1">Andrew Jones</h1>
+                {/* Show this one on mobile (hide at md+)*/}
+                <picture className="block md:hidden">
+                    <img src="prof_pic_sanfran.jpg" alt="My Profile Picture" className="object-scale-down"/>
+                </picture>
+            </div>
+            {/* Show this one on desktop (show at md+)*/}
+            <picture className="hidden md:block">
+                <img src="prof_pic_sanfran.jpg" alt="My Profile Picture" className="object-scale-down"/>
+            </picture>
+        </div>
       </main>
     </div>
   )
